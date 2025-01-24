a822ccb492dbee32e27398ac421c31d8cb6f58b6
Vic CG <vic.cg93@gmail.com>
2025-01-24 02:35:18 -0600

    Initial commit

---
+++ app/src/main/java/com/codelabs/lemonadecompose/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.codelabs.lemonadecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.codelabs.lemonadecompose.ui.theme.LemonadeComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LemonadeComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LemonadeApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LemonImageAndLegend(modifier: Modifier = Modifier) {
+
+    var lemonSqueezCount = 0
+    var lemonSqueezNeeded = 7
+    var legend = "";
+    var imageResource = R.drawable.lemon_tree
+    var imageDescription = ""
+
+    var currentState: Int by remember { mutableStateOf(1) }
+
+    //evalua estado posterior al click
+    when {
+        currentState == 2 -> {
+            imageResource = R.drawable.lemon_squeeze
+            imageDescription = "Lemon to squeeze"
+            legend = "Keep tapping the lemon to squeeze it"
+        }
+        currentState == 3 -> {
+            imageResource = R.drawable.lemon_drink
+            imageDescription = "Lemonade to drink"
+            legend = "Tap the lemonade to drink it"
+        }
+        currentState == 4 -> {
+            imageResource = R.drawable.lemon_restart
+            imageDescription = "empty glass"
+            legend = "Tap the empty glass to start again"
+        }
+        else -> {
+            imageResource = R.drawable.lemon_tree
+            imageDescription = "Lemon tree"
+            legend = "Tap the lemon tree to select a lemon"
+        }
+    }
+
+    Column (modifier = modifier) {
+        Button(onClick = {
+            if(currentState == 4){
+                currentState = 1
+            }else if (currentState == 2){
+                if(lemonSqueezCount == 0){
+                    lemonSqueezNeeded = (2..4).random()
+                }
+                lemonSqueezCount++
+                if(lemonSqueezCount == lemonSqueezNeeded) {
+                    currentState = 3
+                    lemonSqueezCount = 0
+                }
+            }else{
+                currentState++
+            }
+        }) {
+            Image(
+                painter = painterResource(imageResource),
+                contentDescription = imageDescription
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = legend,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LemonadeApp() {
+    LemonImageAndLegend(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center))
+}
+
