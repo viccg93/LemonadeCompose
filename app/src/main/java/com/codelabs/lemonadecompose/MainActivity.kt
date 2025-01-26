3a94f0964bde68ee9adc84d06f3a9fa4412afbae
Vic CG <vic.cg93@gmail.com>
2025-01-26 00:56:00 -0600

    lemonade app completada

--- app/src/main/java/com/codelabs/lemonadecompose/MainActivity.kt
+++ app/src/main/java/com/codelabs/lemonadecompose/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,7 +48,7 @@ class MainActivity : ComponentActivity() {
 fun LemonImageAndLegend(modifier: Modifier = Modifier) {
 
     var lemonSqueezCount = 0
-    var lemonSqueezNeeded = 7
+    var lemonSqueezNeeded = 0
     var legend = "";
     var imageResource = R.drawable.lemon_tree
     var imageDescription = ""
@@ -74,7 +80,16 @@ fun LemonImageAndLegend(modifier: Modifier = Modifier) {
     }
 
     Column (modifier = modifier) {
-        Button(onClick = {
+        Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            colors = ButtonColors(
+                containerColor = Color(android.graphics.Color.parseColor("#99c2a4")),
+                contentColor = Color.Unspecified,
+                disabledContentColor = Color.Gray,
+                disabledContainerColor = Color.Gray
+            ),
+            shape = RoundedCornerShape(16.dp),
+            onClick = {
             if(currentState == 4){
                 currentState = 1
             }else if (currentState == 2){
@@ -108,9 +123,23 @@ fun LemonImageAndLegend(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun TopBar(modifier: Modifier = Modifier){
+    Column (modifier = modifier, verticalArrangement = Arrangement.Center) {
+        Text(
+            modifier = Modifier.padding(top = 30.dp),
+            text = "Lemonade App",
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun LemonadeApp() {
+    TopBar(Modifier.background(Color.Yellow).fillMaxWidth().height(80.dp))
     LemonImageAndLegend(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center))
 }
 
